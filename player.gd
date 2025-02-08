5209f24a8555a6da539482c2d77b633c90044dad
Enginaaron <aarondam@hotmail.com>
2025-02-08 02:53:09 -0500

    moved code into script
    yep

--- player.gd
+++ player.gd
@@ -1,25 +1,51 @@
-extends CharacterBody2D
+extends Sprite2D
+
+@onready var tileMap = $"../TileMapLayer"
+@onready var sprite2D = $Sprite2D
+
+var isMoving = false
+
+func _physics_process(delta):
+	if isMoving == false:
+		return
+		
+	if global_position == sprite2D.global_position:
+		isMoving = false
+		return
+	
+	sprite2D.global_position = sprite2D.global_position.move_toward(global_position, 2)
+
+func _process(delta: float) -> void:
+	# block inputs if player is alr moving
+	if isMoving:
+		return
+		
+	if Input.is_action_pressed("up"):
+		move(Vector2.UP)
+	elif Input.is_action_pressed("down"):
+		move(Vector2.DOWN)
+	elif Input.is_action_pressed("left"):
+		move(Vector2.LEFT)
+	elif Input.is_action_pressed("right"):
+		move(Vector2.RIGHT)
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func move(direction: Vector2):
+	# get current tile Vector2i
+	var currentTile: Vector2i = tileMap.local_to_map(global_position)
+	# get target tile Vector2i
+	var targetTile: Vector2i = Vector2i(
+		currentTile.x + direction.x,
+		currentTile.y + direction.y,
+	)
+	prints("currently at",currentTile," -----  next is", targetTile);
+	# get custom data layer from target file (see if walkable)
+	var tileData: TileData = tileMap.get_cell_tile_data(targetTile)
+	
+	if tileData.get_custom_data("walkable") == false:
+		return
+	
+	# execute player movement
+	isMoving = true
+	global_position = tileMap.map_to_local(targetTile)
+	sprite2D.global_position = tileMap.map_to_local(currentTile);
