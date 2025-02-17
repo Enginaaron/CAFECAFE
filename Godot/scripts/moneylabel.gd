907766b8f6f5bc0eab6391045689b44173025fd5
joelmathewsssss <joelalexmat@gmail.com>
2025-02-16 21:41:36 -0500

    Revert "Merge branch 'main' of https://github.com/Enginaaron/Cooking-Craze"
    This reverts commit 360b2756d7358b884998e518d99b7d8b9d082ab6, reversing
    changes made to df17cdd7dc032deb91935402f0a5f46689f97c74.

--- Godot/scripts/moneylabel.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-var money: int = 0
-
-# Correct update function
-func update_money(amount: int):
-	money += amount
-	self.text = "$" + str(money)  # Use self.text to update this label
