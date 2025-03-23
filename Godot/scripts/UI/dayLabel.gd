7851ae6aa36955ad6e9c6eee64a584dbbdcbf811
Enginaaron <aarondam@hotmail.com>
2025-03-23 04:04:10 -0400

    replaced day text with sun

--- Godot/scripts/UI/dayLabel.gd
+++ Godot/scripts/UI/dayLabel.gd
@@ -1,5 +1,8 @@
 extends Label
 
+@onready var sprite = $"../sprite"
+@export var sun: Texture
+
 var storeInterface
 var main
 var orderCount
@@ -8,7 +11,8 @@ signal day_changed
 var dayCount: int = 0
 
 func _ready() -> void:
-	self.text = "Day "+str(dayCount)
+	self.text = str(dayCount)
+	sprite.texture = sun
 	main = get_node("/root/Node2D")
 	storeInterface = get_node("/root/Node2D/UI/storeInterface")
 
@@ -19,7 +23,7 @@ func order_done() -> void:
 
 func update_day() -> void:
 	dayCount += 1
-	self.text = "Day " + str(dayCount)
+	self.text = str(dayCount)
 	orderCount = main.table_customers.size()
 	storeInterface.refresh_stock()
 	day_changed.emit()
